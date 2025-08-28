9da6952b7d392d03cf69553c1ee63185d849db1f
Jairo <jrwc1989@gmail.com>
2025-08-28 15:32:34 -0600

    Se actualiza componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import { Outlet } from "react-router";
 import { CustomNavBar } from './components/Base/CustomNavBar';
 import './App.css'
+import { CustomNav } from "./components/Base/CustomNav";
+
+const navItems = [
+  {
+    name: "Users",
+    url: "/users",
+  },
+  {
+    name: "To-Do List",
+    url: "/todos",
+  },
+  {
+    name: "Contact",
+    url: "#",
+  },
+  {
+    name: "About",
+    url: "#",
+  },
+];
 
 export default function App() {
   return (
     <>
       <header>
-        <CustomNavBar />
+        <CustomNav navItems={navItems} />
       </header>
 
       {/* Aqui se renderizan todas las ventanas */}
