abe945d49b57a4c3afbd6fabfa8e886a95962ccc
Jairo <jrwc1989@gmail.com>
2025-08-28 15:31:04 -0600

    Se actualian etiquetas Link

--- src/components/Base/CustomNavBar.jsx
+++ src/components/Base/CustomNavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink } from "react-router";
+import { Link } from "react-router";
 import vite from "/vite.svg";
 
 export const CustomNavBar = () => {
@@ -16,12 +16,12 @@ export const CustomNavBar = () => {
 
     return (
         <>
-            <nav className="bg-white border-gray-200 dark:bg-orange-600 rounded-3xl">
+            <nav className="bg-white border-gray-200 dark:bg-orange-600">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <NavLink to={"/"} className={"flex items-center space-x-3 rtl:space-x-reverse"}>
+                    <Link to={"/"} className={"flex items-center space-x-3 rtl:space-x-reverse"}>
                         <img src={vite} className="h-8" alt="Flowbite Logo" />
                         {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">App</span> */}
-                    </NavLink>
+                    </Link>
                     <div className="flex md:order-2">
                         <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -41,9 +41,9 @@ export const CustomNavBar = () => {
                             <li>
                                 {navItems.map((item) => {
                                     return (
-                                        <NavLink className={"py-2 px-3 text-xl text-white font-semibold rounded-lg hover:bg-orange-700"} key={item.name} to={item.url}>
+                                        <Link className={"py-2 px-3 text-xl text-white font-semibold rounded-lg hover:bg-orange-700"} key={item.name} to={item.url}>
                                             {item.name}
-                                        </NavLink>
+                                        </Link>
                                     );
                                 })}
                             </li>
