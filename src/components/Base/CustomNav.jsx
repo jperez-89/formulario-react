49695e4bf30c7846b5538bfd3a67131b791d1cc1
Jairo <jrwc1989@gmail.com>
2025-08-29 09:15:49 -0600

    Se agrega autentificacion

--- src/components/Base/CustomNav.jsx
+++ src/components/Base/CustomNav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router'
 
 
-export const CustomNav = ({ navItems }) => {
+export const CustomNav = ({ navItems, handleLogOut }) => {
     return (
         <nav className="bg-white border-gray-200 dark:bg-orange-600">
             <div className="flex flex-wrap items-center justify-between mx-auto p-4">
@@ -11,9 +11,9 @@ export const CustomNav = ({ navItems }) => {
                     {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">React</span> */}
                 </Link>
                 <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                    <button onClick={handleLogOut} type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                         <span className="sr-only">Open user menu</span>
-                        <img className="w-8 h-8 rounded-full" src="/public/avatar.webp" alt="user photo"></img>
+                        <img className="w-8 h-8 rounded-full" src="/avatar.webp" alt="user photo"></img>
                     </button>
                     {/* <!-- Dropdown menu --> */}
                     <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
